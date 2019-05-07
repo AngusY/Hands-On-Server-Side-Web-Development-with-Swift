32cb3c29433dc3e28a42b4652db9df83bd38ef30
Neil Yeung <neil.y.yeung@gmail.com>
2019-05-07 15:06:19 -0700

    Update to the newer versions
    Update to the newer versions to support Swift 5.0

--- ch09_Authentication_Vapor/myJournal/Package.swift
+++ ch09_Authentication_Vapor/myJournal/Package.swift
@@ -5,17 +5,17 @@ let package = Package(
     name: "myJournal",
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.3.0"),
         
         // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
         //.package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0"),
         
         // Authentication
-        .package(url: "https://github.com/vapor/auth.git", from: "2.0.0"),
+        .package(url: "https://github.com/vapor/auth.git", from: "2.0.3"),
         
         // Leaf template engine
-        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0")
+        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.2")
     ],
     targets: [
     .target(name: "App", dependencies: ["Authentication",
